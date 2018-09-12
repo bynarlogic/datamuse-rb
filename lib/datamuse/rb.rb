c81a7f6bc1f34cbed5fe175aa46f255b86497740
Joshua Jarvis <Joshua Jarvis>
2018-09-11 23:07:21 -0400

    Initial project.

---
+++ lib/datamuse/rb.rb
@@ -0,0 +1,7 @@
+require "datamuse/rb/version"
+
+module Datamuse
+  module Rb
+    # Your code goes here...
+  end
+end
---
+++ lib/datamuse/rb/version.rb
@@ -0,0 +1,5 @@
+module Datamuse
+  module Rb
+    VERSION = "0.1.0"
+  end
+end

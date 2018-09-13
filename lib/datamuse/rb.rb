b66ba1e446671d8eec8fb81e0cb008a68c010468
Joshua Jarvis <Joshua Jarvis>
2018-09-12 22:54:54 -0400

    Added spelled_like and sounds_like. All methods now return a collection of open structs.

--- lib/datamuse/rb.rb
+++ lib/datamuse/rb.rb
@@ -1,5 +1,6 @@
 require "datamuse/rb/version"
 require 'httparty'
+require 'ostruct'
 
 class DatamuseRequest
   include HTTParty
@@ -7,12 +8,20 @@ class DatamuseRequest
 
   def self.send(endpoint,**queries)
     response = get(endpoint,query: queries)
-    response.parsed_response
+    response.parsed_response.map {|r| OpenStruct.new(r)}
   end
 end
 
 class String
   def means_like
-    r = DatamuseRequest.send("/words",{ml: self})
+    DatamuseRequest.send("/words",{ml: self})
+  end
+
+  def sounds_like
+    DatamuseRequest.send("/words",{sl: self})
+  end
+
+  def spelled_like
+    DatamuseRequest.send("/words",{sp: self})
   end
 end
--- spec/datamuse/rb_spec.rb
+++ spec/datamuse/rb_spec.rb
@@ -5,13 +5,83 @@ RSpec.describe Datamuse::Rb do
   end
 
   describe "means_like" do
-    it "returns a collection of words" do
-      expect("ruby".means_like).to be_a(Array)
+    it "returns a collection of words as openstructs" do
+      result = "ruby".means_like
+      expect(result).to be_a(Array)
+      expect(result.first).to be_a(OpenStruct)
+    end
+
+    it "returns empty if the word is not in datamuse" do
+      expect("jkl;asdf".means_like).to be_empty
+    end
+
+    it "returns empty if the string is blank" do
+      expect("".means_like).to be_empty
     end
 
     it "each entry has 3 keys - word, score, and tags" do
       result = "ruby".means_like
-      expect(result.first.keys).to eq(["word","score","tags"])
+      expect(result.first.instance_variable_get("@table").keys).to include(:word,:score,:tags)
     end
   end
+
+
+  describe "sounds_like" do
+    it "returns a collection of words as openstructs" do
+      result = "ruby".sounds_like
+      expect(result).to be_a(Array)
+      expect(result.first).to be_a(OpenStruct)
+    end
+
+    it "returns empty if the string is blank" do
+      expect("".sounds_like).to be_empty
+    end
+
+    it "each entry has 3 keys - word, score, and numSyllables" do
+      result = "ruby".sounds_like
+      expect(result.first.instance_variable_get("@table").keys).to include(:word,:score,:numSyllables)
+    end
+  end
+
+  describe "spelled_like" do
+    it "returns a collection of words as openstructs" do
+      result = "ruby".spelled_like
+      expect(result).to be_a(Array)
+      expect(result.first).to be_a(OpenStruct)
+    end
+
+    it "returns empty if the word is not in datamuse" do
+      expect("jkl;asdf".spelled_like).to be_empty
+    end
+
+    it "returns empty if the string is blank" do
+      expect("".spelled_like).to be_empty
+    end
+
+    it "each entry has 2 keys - word and score" do
+      result = "ruby".spelled_like
+      expect(result.first.instance_variable_get("@table").keys).to include(:word,:score)
+    end
+  end
+
+ # describe "related_word(word)" do
+ #   it "returns a collection of words as openstructs" do
+ #     result = "ruby".spelled_like
+ #     expect(result).to be_a(Array)
+ #     expect(result.first).to be_a(OpenStruct)
+ #   end
+
+ #   it "returns empty if the word is not in datamuse" do
+ #     expect("jkl;asdf".spelled_like).to be_empty
+ #   end
+
+ #   it "returns empty if the string is blank" do
+ #     expect("".spelled_like).to be_empty
+ #   end
+
+ #   it "each entry has 2 keys - word and score" do
+ #     result = "ruby".spelled_like
+ #     expect(result.first.instance_variable_get("@table").keys).to include(:word,:score)
+ #   end
+ # end
 end
